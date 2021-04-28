12e976ab79018e287b50ac27550aca4cb3df4100
jppgomes <joaok8@gmail.com>
2021-04-27 21:51:42 -0300

    Dockerfile e compose api com requirements

--- api/api/settings.py
+++ api/api/settings.py
@@ -80,11 +80,11 @@ WSGI_APPLICATION = 'api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myproject',
-        'USER': 'myuser',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
