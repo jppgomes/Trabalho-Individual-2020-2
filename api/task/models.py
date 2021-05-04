3a2cfd3d1151593ab990f194d4309f324cda240b
jppgomes <joaok8@gmail.com>
2021-05-04 20:15:21 -0300

    fix errors flake8

--- api/task/models.py
+++ api/task/models.py
@@ -1,9 +1,28 @@
+"""
+api mode.
+
+model objects.
+"""
+
+
 from django.db import models
 
+
 class Task(models.Model):
+    """
+    Model Task.
+
+    model task object.
+    """
+
     title = models.CharField("Title", max_length=240)
     description = models.CharField("Title", max_length=240)
     created = models.DateField(auto_now_add=True)
 
     def __str__(self):
+        """
+        str.
+
+        convert so str.
+        """
         return self.name
